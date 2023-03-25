f1cf0d3c186fc90a35c924470c8e217f21072d73
PazLieghton <105952757+PazLieghton@users.noreply.github.com>
2023-03-25 17:18:02 -0300

    finalsabado

--- StructUnions/SimpleListMaker.c
+++ StructUnions/SimpleListMaker.c
@@ -1,63 +1,53 @@
-/*Struct with Student Number, Name, and average score, will write to a file*/
-
+// STRUCTS EJ - 1//
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct list
-{	int snumber;//Student Number//
-	char name[20];
-	float score;
-}l;
-
-
-/*This function will receive a struct and show their contents*/
-
-void show_struct (struct list list) {
-	printf("Student Number: %d - Name: %s - Score %.2f",list.snumber, list.name, list.score);
-}
-
-
-/*This function will travel a struct and travel through it*/
-
-void show_everything(struct list list[], int amount){
-	int i;
-	for (i = 1 ; i < amount + 1 ; i++){
-	show_struct(list[i]);
-	}
-}
-
-
-int main()
+//ESCRITURA//
+struct lista
 {
-	int n, i;
-	printf("Please submit the amount of students: \n");
-	scanf ("%d", &n);
-	
-	struct list l[n];
-	FILE *outfile;
-	outfile = fopen ("students.dat", "w");
-	if (outfile == NULL){
-		fprintf(stderr, "\nOPENERROR\n");
-		exit(1);
-	}
-	
+    int legajo;
+    char nombre[20];
+    float promedio;
+}s;//Hago Typedef s//
 
-	for ( i = 1; i < n + 1; i++){
-		printf("Student Number:\n");
-	        scanf("%d", &l[i].snumber);
-	        printf("Name:\n");
-	      	scanf("%s", &l[i].name);
-	        printf("\nScore:\n");
-	        scanf("%f", &l[i].score);
-	        fwrite (&l, sizeof(l),1,outfile);
-	}
-	if (fwrite != 0)
-		printf("DONE\n");
-	else
-		printf("WERROR\n");
-	show_everything(l,n);
-	fclose (outfile);
-	return 0;
+
+int main ()
+{
+    int n, i;
+    float prom;
+    char nom[100];
+    FILE *outfile;
+    //Fopen Estudiantes.dat//
+    outfile = fopen ("estudiantes.dat", "w");
+    if (outfile == NULL)
+    {
+        fprintf(stderr, "\nOPENERROR\n");
+        exit (1);
+    }
+
+    printf("Ingrese cantidad de alumnos: \n");
+    scanf("%d", &n);
+    printf("....\n");
+
+    for (i = 1; i < n + 1; i++)
+    {
+        printf("Legajo:\n");
+        scanf("%d", &s.legajo);
+        printf("Nombre:\n");
+        scanf("%s", &s.nombre);
+        printf("\nPromedio:\n");
+        scanf("%f", &s.promedio);
+        printf("\n....");
+        printf("\nLegajo: %d Nombre : %s Promedio %f\n",s.legajo,s.nombre,s.promedio);
+        fwrite (&s, sizeof(s), 1, outfile);
+    }
+
+    if(fwrite != 0)
+        printf("LISTO!\n");
+    else
+        printf("WERROR\n");
+
+    fclose (outfile);
+    return 0;
 }
-
--- StructUnions/SimpleListMakerReader.c
+++ StructUnions/SimpleListMakerReader.c
@@ -1,29 +1,65 @@
-/*Struct with Student Number, Name, and average score FILE READER */
-
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 1
 
-struct list
-{	int snumber;//Student Number//
-	char name[20];
-	float score;
+
+struct person
+{
+    int legajo;
+    char nombre[20];
+    float promedio;
 };
 
-
-int main()
+int main ()
 {
-	int n, i;
-	struct list l;
-	FILE *infile;
-	infile = fopen ("students.dat", "r");
-	if (infile == NULL){
-		fprintf(stderr, "\nOPENERROR\n");
-		exit(1);
-	}
-	while (fread(&l, sizeof (struct list),1, infile)){
-		printf("Student Number: %d \\ Name: %s \\ Score: %.2f\n", l.snumber, l.name, l.score);
-		}
-	fclose (infile);
-	return 0;
-}
+    FILE *infile;
+    struct person input;
+    struct person arr[MAX];
+    int i, n = 0, max;
+    float arr1[100];
+    infile = fopen ("estudiantes.dat", "r");
+    if (infile == NULL)
+    {
+        fprintf(stderr, "\nError opening file\n");
+        exit (1);
+    }
+    printf("Los datos son los siguientes: \n");
+    printf("LEGAJO || NOMBRE || PROMEDIO\n");
 
+    while(fread(&arr, sizeof(struct person), 1, infile))
+        for(i = 0; i < MAX; i++ )
+            {
+            printf("%d      %s      %.2f\n",arr[i].legajo, arr[i].nombre, arr[i].promedio);
+            //arr1 copia del texto a local//
+            arr1[n] = arr[i].promedio;
+            n++;
+            }
+    fclose (infile);
+    //Cerramos el archivo y hallamos el maximo del array//
+    for(i = 0; i < n; i++ )
+    {
+        if (arr1[i] > max)
+        {
+            max  = arr1[i];
+        }
+    }
+    //Para volver a trabajar con el struct debemos abrir El texto De nuevo//
+    infile = fopen ("estudiantes.dat", "r");
+    if (infile == NULL)
+    {
+        fprintf(stderr, "\nError opening file\n");
+        exit (1);
+    }
+    printf("El elemento maximo del struct es: %d.\n", max);
+    //Ahora retornamos El Valor Mas alto Y quien le coresponde//
+    while(fread(&arr, sizeof(struct person), 1, infile))
+        for(i = 0; i < MAX; i++ )
+        {
+            if (arr[i].promedio == max)
+            {
+                printf("\n%s tiene el promedio mas alto, y su promedio es: %.2f",arr[i].nombre,arr[i].promedio);
+            }
+        }
+    fclose (infile);
+    return 0;
+}
