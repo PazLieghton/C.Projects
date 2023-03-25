ab85f2e725bfa18a18d5522d7479f099a0daf0ba
PazLieghton <105952757+PazLieghton@users.noreply.github.com>
2023-03-25 11:43:52 -0300

    updafte

--- StructUnions/SimpleListMaker.c
+++ StructUnions/SimpleListMaker.c
@@ -1,8 +1,9 @@
-//Struct with Student Number, Name, and average score, will write to a file//
+/*Struct with Student Number, Name, and average score, will write to a file*/
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX 1000
 
 struct list
 {	int snumber;//Student Number//
@@ -10,19 +11,53 @@ struct list
 	float score;
 }l;
 
+
+/*This function will receive a struct and show their contents*/
+
+void show_struct (struct list list) {
+	printf("Student Number: %d - Name: %s - Score %.2f",list.snumber, list.name, list.score);
+}
+
+
+/*This function will travel a struct and travel through it*/
+
+void show_everything(struct list list[], int amount){
+	int i;
+	for (i = 1 ; i < amount + 1 ; i++){
+	show_struct(list[i]);
+	}
+}
+
+
 int main()
 {
 	int n, i;
-	file *outfile;
+	struct list l[MAX];
+	FILE *outfile;
 	outfile = fopen ("students.dat", "w");
 	if (outfile == NULL){
-		printf(stderr, "\nOPENERROR\n");
+		fprintf(stderr, "\nOPENERROR\n");
 		exit(1);
 	}
 	
 	printf("Please submit the amount of students: \n");
 	scanf ("%d", &n);
 	
+	for ( i = 1; i < n + 1; i++){
+		printf("Student Number:\n");
+	        scanf("%d", &l[i].snumber);
+	        printf("Name:\n");
+	      	scanf("%s", &l[i].name);
+	        printf("\nScore:\n");
+	        scanf("%f", &l[i].score);
+	        fwrite (&l, sizeof(l),1,outfile);
+	}
+	if (fwrite != 0)
+		printf("DONE\n");
+	else
+		printf("WERROR\n");
+	show_everything(l,n);
+	fclose (outfile);
 	return 0;
 }
 
