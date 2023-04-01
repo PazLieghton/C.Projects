2b8825b582f320c32deff576ff5eb318096bf6d7
PazLieghton <105952757+PazLieghton@users.noreply.github.com>
2023-04-01 10:31:24 -0300

    fin

--- Pointers/PointerTesting.c
+++ Pointers/PointerTesting.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
 
+int main (){
+	
+	printf("1 - Changing a value using pointers\n");
+	
+	/*I will try to change the value of i to 75 using pointers*/
+	int *p;
+	int i;
+	int k;
+	i = 42;
+	k = i;
+	p = &i;
+	
+	/*The following pointer changes i*/
+	
+	*p = 75;
+	
+	printf("%d the value of ""i""\n\n",i); 
+	
+	
+
+
+	return 0;
+}
+
