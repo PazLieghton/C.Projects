16bd6c8e3376bbf57be7a2405e479e8d4724388b
PazLieghton <105952757+PazLieghton@users.noreply.github.com>
2023-05-14 13:38:48 -0300

    pain

---
+++ Tests/Test1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+
+{
+	int op;
+	do{
+		printf("\n1. Cargar Archivo\n2. Ingresar Pedido\n3. Mostrar\n4. Actualizar Precio\n");
+		printf("5. Recalcular\n6. Salir\nIngrese opcion:  ");
+		scanf ("%d", &op);
+		switch(op){
+		case 1:
+			break;
+
+		case 2:
+			break;
+
+		case 3:
+			break;
+
+		case 4:
+			break;
+
+		case 5:
+			break;
+
+		case 6:
+			break;
+
+		default:
+			printf("Opcion Invalida\n");
+
+		}
+
+
+
+
+
+	}while (op != 6);
+
+	return 0;
+}
