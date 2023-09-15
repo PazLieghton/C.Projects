79c5281b223179f3b1c5a27064586901c237f502
PazLieghton <105952757+PazLieghton@users.noreply.github.com>
2023-09-15 19:14:23 -0300

    :kissing_smiling_eyes:

--- EntregaFinalPaz/pazlieghton.c
+++ EntregaFinalPaz/pazlieghton.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//Segmentento Primera Opcion. 1//
 
-//Simplemente enlazada Pide//
+// Define una estructura de nodo para la lista enlazada para almacenar caracteres y posiciones.
+typedef struct nodo {
+    unsigned char vectorchar; // Carácter
+    int posicion;             // Posición
+    struct nodo* siguiente;   // Puntero al siguiente nodo
+} nodo;
 
+// Define una estructura de lista enlazada para llevar un registro de los primeros y últimos nodos.
+typedef struct {
+    nodo* primero; // Puntero al primer nodo
+    nodo* ultimo;  // Puntero al último nodo
+} ListaEnlazada;
 
-typedef struct vector {
-    unsigned char vectorchar[100];
-    int posicion;
-    struct vector* ant, * sig;
-} vector;
-
-typedef struct ldoble {
-    vector* prim, * ult;
-} lista;
-
-void insertar_fifo(lista* l, unsigned char vectorchar, int posicion) {
-    vector* nuevoNodo = (vector*)malloc(sizeof(vector));
-    nuevoNodo->vectorchar[0] = vectorchar;
+// Función para insertar un nuevo nodo al final de la lista enlazada (FIFO).
+void insertar_fifo(ListaEnlazada* lista, unsigned char vectorchar, int posicion) {
+    // Asigna memoria para un nuevo nodo.
+    nodo* nuevoNodo = (nodo*)malloc(sizeof(nodo));
+    if (nuevoNodo == NULL) {
+        printf("Fallo en la asignación de memoria.\n");
+        exit(1);
+    }
+    // Establece el carácter, la posición y el puntero siguiente del nuevo nodo.
+    nuevoNodo->vectorchar = vectorchar;
     nuevoNodo->posicion = posicion;
-    nuevoNodo->ant = NULL;
-    nuevoNodo->sig = NULL;
+    nuevoNodo->siguiente = NULL;
 
-    if (l->prim == NULL) {
-        l->prim = nuevoNodo;
-        l->ult = nuevoNodo;
+    // Si la lista está vacía, hace que el nuevo nodo sea el primero y el último.
+    if (lista->primero == NULL) {
+        lista->primero = nuevoNodo;
+        lista->ultimo = nuevoNodo;
     } else {
-        nuevoNodo->ant = l->ult;
-        l->ult->sig = nuevoNodo;
-        l->ult = nuevoNodo;
+        // De lo contrario, agrega el nuevo nodo al final de la lista y actualiza el puntero último.
+        lista->ultimo->siguiente = nuevoNodo;
+        lista->ultimo = nuevoNodo;
     }
 }
 
-
-void mostrarLista(lista l) {
-    vector* actual = l.prim;
+// Función para mostrar el contenido de la lista enlazada.
+void mostrarListaEnlazada(ListaEnlazada* lista) {
+    // Comienza desde el primer nodo.
+    nodo* actual = lista->primero;
 
     if (actual == NULL) {
         printf("La lista está vacía.\n");
@@ -43,100 +50,56 @@ void mostrarLista(lista l) {
     }
 
     printf("\nContenido de la lista:\n");
+    // Recorre la lista e imprime el carácter y la posición de cada nodo.
     while (actual != NULL) {
-        printf("Vector: %c, Posición: %d\n", actual->vectorchar[0], (actual->posicion)+1);
-        actual = actual->sig;
+        printf("Vector: %c, Posición: %d\n", actual->vectorchar, actual->posicion + 1);
+        actual = actual->siguiente;
     }
 }
-
-//Funciones Case 2
-
-void mostrarListaASCII(lista l) {
-    vector* actual = l.prim;
-
-    if (actual == NULL) {
-        printf("La lista está vacía.\n");
-        return;
-    }
-
-    printf("\nContenido de la lista CON MINUSCULAS!:\n");
-    while (actual != NULL) {
-        if  (actual -> vectorchar[0] >= 97){
-            printf("Letra : %c, ASCII: %hhu \n", actual->vectorchar[0], actual -> vectorchar[0]);
-        }
-        actual = actual->sig;
-    }
-
-
-}
-
-
-
-//Fin Case 2//
-
-//Funciones Case 3//
-
-
-
-//Fin Case 3//
-
-
-//Funcion Destruir//
-
-
 
 int main() {
     int num;
-    int salida = 0;
-    const char *cad = "El triunfo fue PERDER, la derrota fue GANAR.";
-    unsigned char *p = (unsigned char*) cad;
+    int salir = 0;
+    const char* cad = "El triunfo fue PERDER, la derrota fue GANAR.";
+    unsigned char* p = (unsigned char*)cad;
+
     printf("Paz Lieghton Final Agosto 5\n");
 
-    int a;
-    a = strlen(cad);
-    printf("Longitud de los datos: %d\n", a);
+    int longitud;
+    longitud = strlen(cad);
+    printf("Longitud de la cadena de entrada: %d\n", longitud);
 
-    int i;
-    printf("\nEN PRIMERA INSTANCIA ESTOS SON LOS VALORES DEL VECTOR EN unsigned char\n|||||||||||||\n");
-    for (i = 0; i < a; i++) {
-        printf("%c", p[i]);
-    }
+    ListaEnlazada lista;
+    lista.primero = NULL;
+    lista.ultimo = NULL;
 
-
-    lista l;
-    l.prim = NULL;
-    l.ult = NULL;
-
-    while (!salida) {
-        printf("\n\nMenu:\n");
-        printf("1. Guardar los datos de Cad en una lista y luego mostrarla en formato porcentaje c (COMPLETO!)\n");
-        printf("2. Guardar en un archivo filtrado.dat las minusculas (COMPLETO!)\n");
-        printf("3. Mostrar los valores de p con 3 bits en 1 como minimo, su cantidad y el porcentaje de bits en 1(Falta el mascara con 3 bits minimo)\n");
+    while (!salir) {
+        printf("\n\nMenú:\n");
+        printf("1. Almacenar los datos de 'cad' en una lista y mostrarla (COMPLETO!)\n");
+        printf("2. Almacenar letras minúsculas en un archivo 'filtrado.dat' (COMPLETO!)\n");
+        printf("3. Mostrar valores con al menos 3 bits encendidos, su conteo y el porcentaje de bits encendidos (Falta la máscara con 3 bits como mínimo)\n");
         printf("4. Salir\n");
-        printf("Ingrese Numero ");
+        printf("Ingrese un número: ");
         scanf("%d", &num);
 
         switch (num) {
             case 1:
-                printf("\n\nElegiste 1.\n");
-                if (l.prim == NULL) {
-                    printf("\nLa lista esta vacia... creando...\n");
-                    for (i = 0; i < a; i++) {
-                        insertar_fifo(&l, p[i], i);
+                printf("\nElegiste 1.\n");
+                if (lista.primero == NULL) {
+                    printf("\nLa lista está vacía... creando...\n");
+                    for (int i = 0; i < longitud; i++) {
+                        insertar_fifo(&lista, p[i], i);
                     }
-                    mostrarLista(l);
-                }
-                else{
-                    mostrarLista(l); //Si esta llena muestro directamente/
+                    mostrarListaEnlazada(&lista);
+                } else {
+                    mostrarListaEnlazada(&lista);
                 }
                 break;
             case 2:
-                printf("\n\nElegiste 2. Estas son las letras y sus ASCII validos \n");
+                printf("\nElegiste 2. Aquí están las letras minúsculas y sus valores ASCII válidos:\n");
 
-                mostrarListaASCII(l);
-                
-                if (l.prim == NULL) {
-                    printf("La lista generada no existe. Primero debes seleccionar la opción 1 para generarla.\n");
+                if (lista.primero == NULL) {
+                    printf("La lista no ha sido generada. Debes seleccionar la opción 1 primero para generarla.\n");
                     break;
                 }
 
@@ -144,54 +107,52 @@ int main() {
                 if (archivo == NULL) {
                     printf("No se pudo crear el archivo.\n");
                     break;
+                }
+
+                printf("\n\n");
+                nodo* subLista = lista.primero;
+
+                if (subLista == NULL) {
+                    printf("La sublista de letras minúsculas está vacía.\n");
+                }
+
+                while (subLista != NULL) {
+                    if (subLista->vectorchar >= 'a' && subLista->vectorchar <= 'z') {
+                        printf("Almacenado: %c ", subLista->vectorchar);
+                        unsigned char vectorchar = subLista->vectorchar;
+                        fwrite(&vectorchar, sizeof(unsigned char), 1, archivo);
                     }
-                
-                printf ("\n\n");
-                vector* Sublista = l.prim;
-
-                if (Sublista == NULL) {
-                printf("La Sublista minusculas está vacía.\n");
+                    subLista = subLista->siguiente;
                 }
 
-                while (Sublista != NULL) {
-                if  (Sublista -> vectorchar[0] >= 97){
-                    printf("Guardada: %c ", Sublista->vectorchar[0]);
-                    unsigned char vectorchar = Sublista->vectorchar[0];
-                    fwrite(&vectorchar, sizeof(unsigned char), 1, archivo);
-                }
-                Sublista = Sublista->sig;
-                }
-    
-                //fclose(archivo);
-                printf("La sublista ha sido guardada en el archivo 'datos.bin'.\n");
+                fclose(archivo);
+                printf("La sublista de letras minúsculas se ha almacenado en el archivo 'filtrado.dat'.\n");
                 break;
-
             case 3:
-                printf("Este es el vector en binario: \n");
+                printf("Este es el vector en binario:\n");
 
-                vector* actual = l.prim;
-                printf("\n\nElegiste 3. Mostrar aquellos Valores que tengan 3 bits en\n");
-                int contar_0 = 0, contar_1 = 0;
-                while (actual != NULL){
+                nodo* actual = lista.primero;
+                printf("\nElegiste 3. Mostrar valores con al menos 3 bits encendidos.\n");
+                int contar0 = 0, contar1 = 0;
+                while (actual != NULL) {
                     unsigned char mascara = 0b10000000;
-                    printf(" %c - ", actual->vectorchar[0]);
-                    for (i = 0; i < 8;  i++){
-                        printf("%d", (actual->vectorchar[0] & mascara) ? 1 : 0);
-                        if ((actual->vectorchar[0] & mascara))
-                            contar_1++;  // Incrementa contador de bits 1
+                    printf(" %c - ", actual->vectorchar);
+                    for (int i = 0; i < 8; i++) {
+                        printf("%d", (actual->vectorchar & mascara) ? 1 : 0);
+                        if (actual->vectorchar & mascara)
+                            contar1++;
                         else
-                            contar_0++;
+                            contar0++;
                         mascara >>= 1;
-                    //Verifica y muestra cada bit
                     }
-                    actual = actual -> sig;
+                    actual = actual->siguiente;
                 }
-                printf ("\nEn el vector hay %d valores de bits en 1, y %d valores de bit en 0", contar_1, contar_0);
-                printf("\n\n EL PORCENTAJE DE BITS EN 1 PARA TODO EL VECTOR ES %d PORCIENTO", (contar_1 * 100)/(contar_0+contar_1));
+                printf("\nEn el vector, hay %d valores con bits encendidos y %d valores con bits apagados.\n", contar1, contar0);
+                printf("El porcentaje de bits encendidos en todo el vector es del %d por ciento.\n", (contar1 * 100) / (contar0 + contar1));
                 break;
             case 4:
                 printf("\nSaliendo...\n");
-                salida = 1;                
+                salir = 1;
                 break;
             default:
                 printf("\nOPCIÓN INVÁLIDA.\n");
@@ -200,13 +161,15 @@ int main() {
 
         printf("\n");
     }
-    //Borrado Final//
-    printf("\nBORRANDO...\n ... \n ...\n");
-    vector* auxiliar = l.prim;
-    while (auxiliar != NULL) {
-        vector* siguiente = auxiliar->sig;
-        free(auxiliar);
-        auxiliar = siguiente;
+
+    // Limpieza de memoria liberando nodos asignados.
+    printf("\nLIMPIANDO...\n");
+
+    nodo* actual = lista.primero;
+    while (actual != NULL) {
+        nodo* siguiente = actual->siguiente;
+        free(actual);
+        actual = siguiente;
     }
 
     return 0;
